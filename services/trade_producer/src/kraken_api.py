52589ab38179f747b9a5d028de565dd554ba69ad
Alberto Rosas <alberto.rsesc@protonmail.com>
2025-01-10 20:45:45 -0800

    Install ruff and updated Makefile to add lint/formatting commands

--- services/trade_producer/src/kraken_api.py
+++ services/trade_producer/src/kraken_api.py
@@ -1,80 +1,84 @@
 import json
-from typing import List, Dict
-from websocket import create_connection
+from typing import Dict, List
+
 from loguru import logger
+from websocket import create_connection
+
 
 class KrakenWebsocketTradeAPI:
-  URL = 'wss://ws.kraken.com/v2'
+    URL = 'wss://ws.kraken.com/v2'
 
-  def __init__(self, product_id: str):
-    self.product_id = product_id
+    def __init__(self, product_id: str):
+        self.product_id = product_id
 
-    self._ws = create_connection(self.URL)
-    logger.info("Connection Established")
+        self._ws = create_connection(self.URL)
+        logger.info('Connection Established')
 
-    # Subscribe to the trades for the given `product_id`
-    self._subscribe(product_id)
+        # Subscribe to the trades for the given `product_id`
+        self._subscribe(product_id)
 
-  def _subscribe(self, product_id: str):
-      """
-      Establish connection to the Kraken websocket API and subscribe to the trades for the given `product_id`
-      """
+    def _subscribe(self, product_id: str):
+        """
+        Establish connection to the Kraken websocket API and subscribe to the trades for the given `product_id`
+        """
 
-      logger.info(f"Subscribing to trades for {product_id}")
-      # Subscribe to the trades for the given `product_id`
-      message = {
-        "method": "subscribe",
-        "params": {
-          "channel": "trade",
-          "symbol": [
-            product_id,
-          ],
-          "snapshot": False
+        logger.info(f'Subscribing to trades for {product_id}')
+        # Subscribe to the trades for the given `product_id`
+        message = {
+            'method': 'subscribe',
+            'params': {
+                'channel': 'trade',
+                'symbol': [
+                    product_id,
+                ],
+                'snapshot': False,
+            },
         }
-      }
 
-      self._ws.send(json.dumps(message))
+        self._ws.send(json.dumps(message))
 
-      logger.info("Subscription worked!")
+        logger.info('Subscription worked!')
 
-      # Discarding the first 2 messages we got from the websocket, because
-      # they contain no trade data, only confirmation from Kraken that the subscription was successful.
-      _ = self._ws.recv()
-      _ = self._ws.recv()
+        # Discarding the first 2 messages we got from the websocket, because
+        # they contain no trade data, only confirmation from Kraken that the subscription was successful.
+        _ = self._ws.recv()
+        _ = self._ws.recv()
 
-  def get_trades(self) -> List[Dict]:
-    # mock_trades = [
-    #   {
-    #     'product_id': 'BTC-USD',
-    #     'price': 60000,
-    #     'volume': 0.01,
-    #     'timestamp': 1630000000
-    #   },
-    #   {
-    #     'product_id': 'BTC-USD',
-    #     'price': 59000,
-    #     'volume': 0.01,
-    #     'timestamp': 1640000000
-    #   },
-    # ]
+    def get_trades(self) -> List[Dict]:
+        # mock_trades = [
+        #   {
+        #     'product_id': 'BTC-USD',
+        #     'price': 60000,
+        #     'volume': 0.01,
+        #     'timestamp': 1630000000
+        #   },
+        #   {
+        #     'product_id': 'BTC-USD',
+        #     'price': 59000,
+        #     'volume': 0.01,
+        #     'timestamp': 1640000000
+        #   },
+        # ]
 
-    message = self._ws.recv()
+        message = self._ws.recv()
 
-    if 'heartbeat' in message:
-      # when we get a heartbeat, i return empty list.
-      return []
+        if 'heartbeat' in message:
+            # when we get a heartbeat, i return empty list.
+            return []
 
-    # parse message string as a dictionary
-    message = json.loads(message)
-    logger.info('Message received: ', message)
+        # parse message string as a dictionary
+        message = json.loads(message)
+        logger.info('Message received: ', message)
 
-    trades = []
-    for trade in message['data']:
-      trades.append({
-        'product_id': self.product_id,
-        'price': trade['price'],
-        'volume': trade['qty'],
-        'timestamp': trade['timestamp'],
-      })
+        trades = []
+        for trade in message['data']:
+            trades.append(
+                {
+                    'product_id': self.product_id,
+                    'price': trade['price'],
+                    'volume': trade['qty'],
+                    'timestamp': trade['timestamp'],
+                }
+            )
 
-    return trades
+        return trades
--- services/trade_producer/src/main.py
+++ services/trade_producer/src/main.py
@@ -1,62 +1,53 @@
 from typing import Dict, List
+
+from loguru import logger
 from quixstreams import Application
 
 from kraken_api import KrakenWebsocketTradeAPI
-from loguru import logger
+
 
 def produce_trades(
-  kafka_broker_address: str,
-  kafka_topic_name: str,
+    kafka_broker_address: str,
+    kafka_topic_name: str,
 ) -> None:
-  """
-  Reads trades from the Kraken websocket API and saves them into a Kafka Topic.
+    """
+    Reads trades from the Kraken websocket API and saves them into a Kafka Topic.
 
-  Args:
-    kafka_broker_address (str): The address of the Kafka broker.
-    kafka_topic_name (str): The name of the Kafka Topic.
+    Args:
+      kafka_broker_address (str): The address of the Kafka broker.
+      kafka_topic_name (str): The name of the Kafka Topic.
 
-  Returns:
-    None
-  """
+    Returns:
+      None
+    """
 
-  app = Application(broker_address = kafka_broker_address)
+    app = Application(broker_address=kafka_broker_address)
 
-  # The Topic where we will save the Trades.
-  topic = app.topic(
-    name = kafka_topic_name,
-    value_serializer = 'json'
-  )
+    # The Topic where we will save the Trades.
+    topic = app.topic(name=kafka_topic_name, value_serializer='json')
 
-  # Create instance of the Kraken API
-  kraken_api = KrakenWebsocketTradeAPI(product_id = 'BTC/USD')
+    # Create instance of the Kraken API
+    kraken_api = KrakenWebsocketTradeAPI(product_id='BTC/USD')
 
-  # Create a Producer instance
-  with app.get_producer() as producer:
-    while True:
-      # Get the trades from the Kraken API
-      trades : List[Dict] = kraken_api.get_trades()
+    # Create a Producer instance
+    with app.get_producer() as producer:
+        while True:
+            # Get the trades from the Kraken API
+            trades: List[Dict] = kraken_api.get_trades()
 
-      for trade in trades:
-        # Serialize a trade using the defined Topic
-        message = topic.serialize(
-          key = trade["product_id"],
-          value = trade
-        )
+            for trade in trades:
+                # Serialize a trade using the defined Topic
+                message = topic.serialize(key=trade['product_id'], value=trade)
 
-        # Produce a message into the Kafka topic
-        producer.produce(
-            topic = topic.name,
-            value = message.value,
-            key = message.key
-        )
+                # Produce a message into the Kafka topic
+                producer.produce(topic=topic.name, value=message.value, key=message.key)
 
-        logger.info('Message Sent!')
+                logger.info('Message Sent!')
 
-      from time import sleep
-      sleep(1)
+            from time import sleep
+
+            sleep(1)
+
 
 if __name__ == '__main__':
-  produce_trades(
-    kafka_broker_address = "redpanda-0:9092",
-    kafka_topic_name = "trade"
-  )
+    produce_trades(kafka_broker_address='redpanda-0:9092', kafka_topic_name='trade')
